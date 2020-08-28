864fc3536d2a3d6e605a3e22fd8579081540b969
Kedaren <kedaren.dorasami@yocto.re>
2020-08-28 18:18:58 +0400

    first commit

---
+++ blog-backend/index.ts
@@ -0,0 +1,20 @@
+const express = require('express');
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
+const app = express();
+const routes = require("./routes/index");
+
+
+app.get('/', function (req: any, res: any) {
+  res.send('welcome');
+});
+
+app.listen(3000, function() {
+  console.log('App listening on port 3000!');
+});
+
+// use the body parser middleware to allow express to recognize json requests
+app.use(bodyParser.json());
+
+app.use("/routes", routes);
