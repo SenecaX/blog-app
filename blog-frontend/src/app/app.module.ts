4c0316e61c25b5611c7dfa880cf286b09cd24bd3
Kedaren <kedaren.dorasami@yocto.re>
2021-01-01 17:35:47 +0400

    added angular material

--- blog-frontend/src/app/app.module.ts
+++ blog-frontend/src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { MaterialModule } from './shared/material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
+  exports: [MaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
